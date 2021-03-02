395de00fe560768b40bc89f600db9c58a70ee710
Ivan Dramac <ivancadramac@gmail.com>
2021-03-02 19:44:49 +0100

    Adding some comments on the code

--- main.js
+++ main.js
@@ -1,21 +1,23 @@
 /*It is Console game where user and ai bet on Heads or Tails and the coinFliping method "Flips the coin randomly "  on line 46 is the User input
 for Heads or Tails.*/
 
+// Randomly creates number 0 or 1 and returns if 0 = Heads if 1 = Tails
 const coinFliping = () => 
 {
     let coinFlipingNumber = Math.round(Math.random() * 2);
     if(coinFlipingNumber === 0) {return 'Heads';} else {return 'Tails';}
 
 }
-
+// Calling the method above and saving result to a variable
 let ai = coinFliping();
 
+// userinput
 const userInput = (userInput) =>
 {
     return userInput;
 }
 
-
+// Randomly computer input was not neccessery but for practice 
 const computerInput = () =>
 {
     let computer = Math.round(Math.random() * 2);
@@ -23,7 +25,7 @@ const computerInput = () =>
     return 'Tails';
 }
 
-
+// Methods that decides who has won the game .
 const decideWinner = (userInput, computerInput) =>
 {
     
@@ -41,7 +43,7 @@ const decideWinner = (userInput, computerInput) =>
         return 'AI';
     }
 }
-
+// Puting all in playgame together 
 const playGame = () => 
 {
     let user = userInput('Heads');
@@ -51,4 +53,4 @@ const playGame = () =>
     return winnerChosen; 
 }
 
-playGame();
+playGame(); 
